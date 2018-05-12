7c5584ad83e291582147a61a3fee4ad1f4573c69
FarinhoD <diogo.farinhovr@gmail.com>
2018-05-12 17:45:52 -0300

    commit
    commit

---
+++ Royale_Prototype/Assets/Scripts/Weapons/Pistol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapons {
+
+    public Transform shotSpawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(projectile, shotSpawn.position, shotSpawn.rotation);
+        }
+	}
+}
