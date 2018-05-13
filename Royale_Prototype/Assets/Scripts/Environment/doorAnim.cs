c0b9fa81ee0bc7b8dceba5ca647268dd207953da
Jake Moore <jake.moorevr@gmail.com>
2018-05-13 14:32:13 -0300

    Revert "Doors open and close in all directions"
    This reverts commit f821090f2320cfa5c477103856635ca9776eeab1.

--- Royale_Prototype/Assets/Scripts/Environment/doorAnim.cs
+++ Royale_Prototype/Assets/Scripts/Environment/doorAnim.cs
@@ -12,73 +12,43 @@ public class doorAnim : MonoBehaviour {
 	void Start () {
         //get animator on door
         anim = GetComponent<Animator>();
-        
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        //if a character has entered the collider and is pressing E
-        if (allowedToOpen && Input.GetKeyDown(KeyCode.E)) {
+        if (!anim.GetBool("openDoor"))
+        {
+            if (Input.GetKeyDown(KeyCode.E) && allowedToOpen)
+            {
+                anim.SetBool("openDoor", true);
+            }
+        }
 
-            Debug.Log("Pressed E");
-
-            //Inside house, door is open inward.  Close door.
-            if (isInside() && anim.GetBool("openDoorInward"))
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.E) && allowedToOpen)
             {
-                Debug.Log("Inside house, door is open inward.  Close door.");
-                anim.SetBool("openDoorInward", false);
-                anim.SetBool("openDoorOutward", false);
-                anim.SetBool("isInside", true);
-            }
-            //Inside house, door is closed.  Open outward.
-            else if (isInside() && !anim.GetBool("openDoorOutward") && !anim.GetBool("openDoorInward"))
-            {
-                Debug.Log("Inside house, door is closed.  Open outward.");
-                anim.SetBool("openDoorInward", false);
-                anim.SetBool("openDoorOutward", true);
-                anim.SetBool("isInside", true);
-            }
-            //Inside house, door is open outward.  Close door.
-            else if (isInside() && anim.GetBool("openDoorOutward") && !anim.GetBool("openDoorInward"))
-            {
-                Debug.Log("Inside house, door is open outward.  Close door.");
-                anim.SetBool("openDoorInward", false);
-                anim.SetBool("openDoorOutward", false);
-                anim.SetBool("isInside", true);
-            }
-            //Outside house, door is open outward.  Close door.
-            else if (!isInside() && anim.GetBool("openDoorOutward"))
-            {
-                Debug.Log("Outside house, door is open outward.  Close door.");
-                anim.SetBool("openDoorInward", false);
-                anim.SetBool("openDoorOutward", false);
-                anim.SetBool("isInside", false);
-            }
-            //Outside house, door is closed.  Open Inward.
-            else if (!isInside() && !anim.GetBool("openDoorOutward") && !anim.GetBool("openDoorInward"))
-            {
-                Debug.Log("Outside house, door is closed.  Open Inward.");
-                anim.SetBool("openDoorInward", true);
-                anim.SetBool("openDoorOutward", false);
-                anim.SetBool("isInside", true);
+                anim.SetBool("openDoor", false);
             }
         }
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
+        Debug.Log("Can open door");
         if (other.gameObject.tag == "Player")
             allowedToOpen = true;
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
+        Debug.Log("Can no longer open");
         allowedToOpen = false;
     }
 
     private bool isInside ()
     {
-        return roof.GetComponent<HideRoof>().isPlayerInside();
+        return roof.isInside();
     }
 }
