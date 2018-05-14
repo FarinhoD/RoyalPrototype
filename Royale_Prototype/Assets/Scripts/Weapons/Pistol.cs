762da7fe30722cc2c16bdb9d3024db48a5c46ad8
Jake Moore <jake.moorevr@gmail.com>
2018-05-14 00:10:26 -0300

    Reset master to my branches working state

--- Royale_Prototype/Assets/Scripts/Weapons/Pistol.cs
+++ Royale_Prototype/Assets/Scripts/Weapons/Pistol.cs
@@ -6,22 +6,9 @@ public class Pistol : Weapons {
 
     public Transform shotSpawn;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 	// Use this for initialization
 	void Start () {
 		
-=======
-=======
->>>>>>> parent of 763eb22... animation updates, weapons script update, particles, uzi
-
-	// Use this for initialization
-	void Start () {
-        
-<<<<<<< HEAD
->>>>>>> parent of 763eb22... animation updates, weapons script update, particles, uzi
-=======
->>>>>>> parent of 763eb22... animation updates, weapons script update, particles, uzi
 	}
 	
 	// Update is called once per frame
@@ -30,14 +17,7 @@ public class Pistol : Weapons {
         if (Input.GetButton("Fire1") && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
-<<<<<<< HEAD
             Instantiate(projectile, shotSpawn.position, shotSpawn.rotation);
-=======
-            Instantiate(projectile, shotSpawn.position, shotSpawn.rotation );
-<<<<<<< HEAD
->>>>>>> parent of 763eb22... animation updates, weapons script update, particles, uzi
-=======
->>>>>>> parent of 763eb22... animation updates, weapons script update, particles, uzi
         }
 	}
 }
